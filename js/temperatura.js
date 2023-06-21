cfc51c067cc076eb49a80e09579b276ac2468fed
VictorVechi <victorhvechi@gmail.com>
2023-06-21 10:55:48 -0300

    criação da função de conversão

---
+++ js/temperatura.js
@@ -0,0 +1,28 @@
+function converterTemperatura() {
+    let medidaOriginal = document.getElementById("selecao").value
+    let medidaConvertida = document.getElementById("selecaoFinal").value
+    let valor = Number(document.getElementById("valor-inicial").value)
+    let resposta = document.getElementById("resultado")
+
+    if (medidaOriginal == medidaConvertida) {
+        resposta.innerHTML = `Resposta: ${valor}`
+    } else if (medidaOriginal == 1 && medidaConvertida == 2) {
+        let resultado = (valor * 1.8) + 32
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} °F`
+    } else if (medidaOriginal == 1 && medidaConvertida == 3) {
+        let resultado = valor + 273.15
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} K`
+    } else if (medidaOriginal == 2 && medidaConvertida == 1) {
+        let resultado = (valor - 32) / 1.8
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} °C`
+    } else if (medidaOriginal == 2 && medidaConvertida == 3) {
+        let resultado = ((valor - 32) / 1.8) + 273.15
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} K`
+    } else if (medidaOriginal == 3 && medidaConvertida == 1) {
+        let resultado = valor - 273.15
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} °C`
+    } else if (medidaOriginal == 3 && medidaConvertida == 2) {
+        let resultado = ((valor - 273.15) * 1.8) + 32
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} °F`
+    }
+}
