d4ea4aa0e004f4996480fa9a3a4dcb93a03ad4e1
VictorVechi <victorhvechi@gmail.com>
2023-06-21 10:21:14 -0300

    criação da função de conversão

---
+++ js/peso.js
@@ -0,0 +1,28 @@
+function converterPeso() {
+    let medidaOriginal = document.getElementById("selecao").value
+    let medidaConvertida = document.getElementById("selecaoFinal").value
+    let valor = Number(document.getElementById("valor-inicial").value)
+    let resposta = document.getElementById("resultado")
+
+    if (medidaOriginal == medidaConvertida) {
+        resposta.innerHTML = `Resposta: ${valor}`
+    } else if (medidaOriginal == 1 && medidaConvertida == 2) {
+        let resultado = valor * 1000
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} g`
+    } else if (medidaOriginal == 1 && medidaConvertida == 3) {
+        let resultado = valor * 2.205
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} lb`
+    } else if (medidaOriginal == 2 && medidaConvertida == 1) {
+        let resultado = valor / 1000
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} kg`
+    } else if (medidaOriginal == 2 && medidaConvertida == 3) {
+        let resultado = valor / 453.6
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} lb`
+    } else if (medidaOriginal == 3 && medidaConvertida == 1) {
+        let resultado = valor / 2.205
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} kg`
+    } else if (medidaOriginal == 3 && medidaConvertida == 2) {
+        let resultado = valor * 453.6
+        resposta.innerHTML = `Resposta: ${resultado.toFixed(2)} g`
+    }
+}
